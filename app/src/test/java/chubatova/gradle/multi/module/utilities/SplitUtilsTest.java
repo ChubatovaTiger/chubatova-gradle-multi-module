2d9ed57e9a1d60578529c5aa1ef00d091e4b865c
AChubatova <n.chubatova@gmail.com>
2022-03-29 16:14:59 +0200

    add new test

---
+++ app/src/test/java/chubatova/gradle/multi/module/utilities/SplitUtilsTest.java
@@ -0,0 +1,14 @@
+package chubatova.gradle.multi.module.utilities;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class SplitUtilsTest {
+    @Test void testConstructor() {
+
+        assertEquals(0, 0);
+    }
+}
